990bf08f2053bd1d7efa2088dc0655f37ad34628
brndntru <112743187+brndntru@users.noreply.github.com>
2025-02-02 23:19:40 -0800

    Update and rename pascalTriangle.java to pascalsTriangle.java

--- pascalsTriangle.java
+++ pascalsTriangle.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class pascalTriangle {
+public class pascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
         // list to store rows
         List<List<Integer>> triangle = new ArrayList<>();
